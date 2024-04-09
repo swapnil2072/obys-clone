629d4413ca1e808b32ac5a7c52a82a79f85cac58
swapnil2072 <swapnilshinde909693@gmail.com>
2024-04-09 18:58:25 +0530

    created and added favicon

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-let tl = gsap.timeline();
+function loadingAnimation() {
+  let tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from("line1-part1, .line h2", {
-  opacity: 0,
-  onStart: function () {
-    let h5Timer = document.querySelector("#line1-part1 h5");
-    let grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
-        h5Timer.innerHTML = grow++;
-      } else {
-        grow = 100;
-        h5Timer.innerHTML = grow;
-      }
-    }, 20);
-  },
-});
+  tl.from("line1-part1, .line h2", {
+    opacity: 0,
+    onStart: function () {
+      let h5Timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5Timer.innerHTML = grow++;
+        } else {
+          grow = 100;
+          h5Timer.innerHTML = grow;
+        }
+      }, 20);
+    },
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.4,
-  delay: 4,
-});
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.3,
+    delay: 4,
+  });
 
-tl.from("#page1", {
-  delay: 0.2,
-  y: 1600,
-  opacity: 0,
-  duration: 0.6,
-  ease: Power4,
-});
+  tl.from("#page1", {
+    delay: 0.1,
+    y: 1200,
+    opacity: 0,
+    duration: 0.5,
+    ease: Power4,
+  });
 
-tl.to("#loader", { display: "none" });
+  tl.to("#loader", { display: "none" });
+}
+
+loadingAnimation();
